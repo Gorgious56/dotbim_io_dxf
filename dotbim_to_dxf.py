678abfa78ee9447101d139a1ef32a7ca969e116d
Gorgious <nathan.hild@gmail.com>
2022-03-08 14:43:35 +0100

    Set block color according to element color

--- dotbim_to_dxf.py
+++ dotbim_to_dxf.py
@@ -6,7 +6,7 @@ import pyquaternion
 
 
 def dotbim_mesh_to_dxf_mesh(layout, dotbim_mesh):
-    dxf_mesh = layout.add_mesh()
+    dxf_mesh = layout.add_mesh(dxfattribs={"color": 0})  # Color BY BLOCK
     with dxf_mesh.edit_data() as mesh_data:
         mesh_data.vertices = [
             (dotbim_mesh.coordinates[c], dotbim_mesh.coordinates[c + 1], dotbim_mesh.coordinates[c + 2])
@@ -17,6 +17,12 @@ def dotbim_mesh_to_dxf_mesh(layout, dotbim_mesh):
             for i in range(0, len(dotbim_mesh.indices), 3)
         ]
     return dxf_mesh
+
+
+def rgb_to_hex(vals):
+    # Ensure values are rounded integers, convert to hex, and concatenate
+    return int("0x" + "".join(["{:02X}".format(int(round(x))) for x in vals]), 16)
+
 
 def dotbim_to_dxf(dotbim_filepath):
     file = dotbimpy.File.read(dotbim_filepath)
@@ -32,11 +38,23 @@ def dotbim_to_dxf(dotbim_filepath):
         dotbim_mesh_to_dxf_mesh(block_mesh_def, dotbim_mesh)
         for elt in elts:
             block_elt_def = dxf_file.blocks.new(name=elt.info.get("Name", str(elt.guid)))
+            block_elt_def.add_blockref(block_mesh_def.name, insert=(0, 0, 0), dxfattribs={"color": 0})  # Color BY BLOCK
+
             attr_names = list(elt.info.keys())
             attr_names.extend(("guid", "type"))
             for attr_name in attr_names:
                 # Can't seem to make the invisible attribute work. Setting the texts really really small for now
                 block_elt_def.add_attdef(attr_name, dxfattribs={"invisible": True, "height": 0.00001})
+
+            block_elt_instance = dxf_msp.add_blockref(
+                block_elt_def.name,
+                insert=(0, 0, 0),
+                dxfattribs={
+                    "color": 257,  # True Color
+                    "true_color": rgb_to_hex((elt.color.r, elt.color.g, elt.color.b)),
+                    # "transparency": float(elt.color.a / 255),  # Throws DXFValueError. Can't make it work for INSERT ?
+                },
+            )
             matrix = ezdxf.math.Matrix44.translate(elt.vector.x, elt.vector.y, elt.vector.z)
             rot = elt.rotation
             matrix_rotation = pyquaternion.Quaternion(rot.qw, rot.qx, rot.qy, rot.qz).rotation_matrix
