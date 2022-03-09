2610efe2c3afc5da6fec719cb99057170961a3cf
Gorgious <nathan.hild@gmail.com>
2022-03-09 10:39:53 +0100

    Linked meshes not longer nested blocks Instead, all the possible attributes are defined in the base mesh block If an entity doesn't contain an entry for the attribute, it is leaved blank

--- dotbim_to_dxf.py
+++ dotbim_to_dxf.py
@@ -6,8 +6,9 @@ import pyquaternion
 import uuid
 
 
-def add_mesh_to_layout(layout, dotbim_mesh):
-    dxf_mesh = layout.add_mesh(dxfattribs={"color": 0})  # Color BY BLOCK
+def create_blockdef_from_dotbim_mesh(dxf_file, block_name, dotbim_mesh):
+    block_def = dxf_file.blocks.new(name=block_name)
+    dxf_mesh = block_def.add_mesh(dxfattribs={"color": 0})  # Color BY BLOCK
     with dxf_mesh.edit_data() as mesh_data:
         mesh_data.vertices = [
             (dotbim_mesh.coordinates[c], dotbim_mesh.coordinates[c + 1], dotbim_mesh.coordinates[c + 2])
@@ -17,7 +18,7 @@ def add_mesh_to_layout(layout, dotbim_mesh):
             (dotbim_mesh.indices[i], dotbim_mesh.indices[i + 1], dotbim_mesh.indices[i + 2])
             for i in range(0, len(dotbim_mesh.indices), 3)
         ]
-    return dxf_mesh
+    return block_def
 
 
 def rgb_to_hex(vals):
@@ -40,12 +41,6 @@ def get_matrix(dotbim_element):
     return matrix
 
 
-def get_attributes(dotbim_element):
-    attributes = {attrib: value for (attrib, value) in dotbim_element.info.items()}
-    attributes.update({"guid": dotbim_element.guid, "type": dotbim_element.type})
-    return attributes
-
-
 def dotbim_to_dxf(dotbim_filepath):
     dotbim_file = dotbimpy.File.read(dotbim_filepath)
     dxf_file = ezdxf.new(dxfversion="R2010")
@@ -53,31 +48,25 @@ def dotbim_to_dxf(dotbim_filepath):
         dxf_file.header.custom_vars.append(key, value)
     dxf_msp = dxf_file.modelspace()
     meshes_users = defaultdict(list)
+    meshes_attrs = defaultdict(set)
     elt_types = set()
 
     for elt in dotbim_file.elements:
         meshes_users[elt.mesh_id].append(elt)
+        meshes_attrs[elt.mesh_id].update(elt.info.keys())
         elt_types.add(elt.type)
     for layer_name in get_layer_names(elt_types):
         dxf_file.layers.new(layer_name)
     for mesh_id, elts in meshes_users.items():
         dotbim_mesh = next((m for m in dotbim_file.meshes if m.mesh_id == mesh_id), None)
-        block_mesh_def = dxf_file.blocks.new(name=f"Mesh {mesh_id}_{uuid.uuid4()}")
-        add_mesh_to_layout(block_mesh_def, dotbim_mesh)
+        block_name = f"Mesh {mesh_id}_{uuid.uuid4()}"
+        block_def = create_blockdef_from_dotbim_mesh(dxf_file, block_name, dotbim_mesh)
+        for attr_name in meshes_attrs[mesh_id]:
+            # Can't seem to make the invisible attribute work. Setting the texts really really small for now
+            block_def.add_attdef(attr_name, dxfattribs={"invisible": True, "height": 0.00001})
         for elt in elts:
-            if len(elts) == 1:
-                block_elt_def = block_mesh_def
-            else:
-                block_elt_def = dxf_file.blocks.new(name=str(elt.guid))
-                block_elt_def.add_blockref(block_mesh_def.name, insert=(0, 0, 0), dxfattribs={"color": 0})  # BY BLOCK
-
-            attributes = get_attributes(elt)
-            for attr_name in attributes.keys():
-                # Can't seem to make the invisible attribute work. Setting the texts really really small for now
-                block_elt_def.add_attdef(attr_name, dxfattribs={"invisible": True, "height": 0.00001})
-
             block_elt_instance = dxf_msp.add_blockref(
-                block_elt_def.name,
+                block_name,
                 insert=(0, 0, 0),
                 dxfattribs={
                     "color": 257,  # True Color
@@ -86,7 +75,11 @@ def dotbim_to_dxf(dotbim_filepath):
                     # "transparency": float(elt.color.a / 255),  # Throws DXFValueError. Can't make it work for INSERT ?
                 },
             )
+
             block_elt_instance.transform(get_matrix(elt))
+
+            attributes = {attrib: value for (attrib, value) in elt.info.items()}
+            attributes.update({"guid": elt.guid, "type": elt.type})
             block_elt_instance.add_auto_attribs(attributes)
 
     dotbim_path = Path(dotbim_filepath)
