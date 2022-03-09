353ed780458d1fffb124b6390cc10a2f47152862
Gorgious <nathan.hild@gmail.com>
2022-03-09 11:02:59 +0100

    Export MESH objects inside BLOCKs

--- dxf_to_dotbim.py
+++ dxf_to_dotbim.py
@@ -4,11 +4,25 @@ import ezdxf
 
 
 def dxf_to_dotbim(dxf_filepath):
-    dxf_file = ezdxf.readfile(str(dxf_filepath))
-    blocks = dxf_file.blocks
-
     dotbim_meshes = []
     dotbim_elements = []
+
+    dxf_file = ezdxf.readfile(str(dxf_filepath))
+    blocks = dxf_file.blocks
+    for layout in blocks:
+        if not layout.is_block_layout or layout.block.is_anonymous or layout.block.is_xref:
+            continue
+        for entity in layout:
+            if not isinstance(entity, ezdxf.entities.mesh.Mesh):
+                continue
+            faces = []
+            for f in entity.faces:
+                faces.extend((f[0], f[1], f[2]))
+            vertices = []
+            for c in entity.vertices:
+                vertices.extend((c[0], c[1], c[2]))
+            dotbim_meshes.append(dotbimpy.Mesh(mesh_id=len(dotbim_meshes), coordinates=vertices, indices=faces))
+
 
     # file_info = {"Author": author, "Date": date.today().strftime("%d.%m.%Y")}
     dotbim_file = dotbimpy.File("1.0.0", meshes=dotbim_meshes, elements=dotbim_elements, info={})
